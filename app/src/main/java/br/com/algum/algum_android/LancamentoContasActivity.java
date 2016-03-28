dac36ecd7d0b9b81d05d6eaacd311c55912140cb
Andre <andrelrs80@gmail.com>
2016-03-27 23:36:14 -0300

    Initial commit

---
+++ app/src/main/java/br/com/algum/algum_android/LancamentoContasActivity.java
@@ -0,0 +1,13 @@
+package br.com.algum.algum_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LancamentoContasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lancamento_contas);
+    }
+}
